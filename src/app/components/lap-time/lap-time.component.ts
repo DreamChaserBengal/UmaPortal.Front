c02416bfc7e4f08417fb766fdaff5a482a1eb2a1
yamatatsu <t.y.wakataka@icloud.com>
2023-10-09 23:28:03 +0900

    temporary charts

--- src/app/components/lap-time/lap-time.component.ts
+++ src/app/components/lap-time/lap-time.component.ts
@@ -6,6 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lap-time.component.css']
 })
 export class LapTimeComponent implements OnInit {
+  // グラフの表示サイズ
+  view: [number, number] = [800, 450];
+  //色設定
+  colorScheme = {
+    domain: ["#cccccc"],
+  };
+  //チャートに表示するデータ
+  sampleData = [
+    {
+      name: "lap time",
+      series: [
+        { name: "200m", value: 12.2 },
+        { name: "400m", value: 11.2 },
+        { name: "600m", value: 12.3 },
+        { name: "800m", value: 12.3 },
+        { name: "1000m", value: 11.7 },
+        { name: "1200m", value: 12.3 },
+        { name: "1400m", value: 11.8 },
+        { name: "1600m", value: 11.5 },
+        { name: "1800m", value: 11.5 },
+        { name: "2000m", value: 11.8 },
+      ],
+    },
+  ];
+
+  // チャートの設定
+  legend: boolean = false;
+  showLabels: boolean = false;
+  animations: boolean = false;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  autoScale: boolean = true;
+  xAxisLabel: string = "距離";
+  yAxisLabel: string = "タイム";
 
   constructor() { }
 
